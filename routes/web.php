32f754b4b9aad9ffdfe7084497f6c3c2501ed9a7
ibnuzaman <ibnuzaman2133@gmail.com>
2024-04-17 03:29:25 +0700

    Membuat CRUD Produk Sederhana

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -20,5 +21,11 @@ Route::middleware(['auth', 'access-level:admin'])->group(function () {
     Route::get('/admin/dashboard', [HomeController::class, 'dashboard'])->name('admin.dashboard');
 });
 
+Route::get('/forgot-password', function () {
+    return view('auth.forgot-password');
+})->middleware(['auth', 'access-level:user'])->name('password.request');
+
 // default Laravel home route
 Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::resource('products', ProductController::class);
